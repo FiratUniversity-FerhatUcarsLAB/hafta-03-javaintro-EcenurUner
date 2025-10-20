fb8c3e7acff65fad1282d04e43263f1a6bac9e75
EcenurUner <250541078@firat.edu.tr>
2025-10-20 16:32:14 +0300

    Add files via upload

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,8 @@
+ public class UcMesaj {
+
+    public static void main(String[] args){
+     System.out.println("Java'ya Hos Geldiniz");
+     System.out.println("Bilgisayar Bilimlerine Hos Geldiniz");
+     System.out.println("Proglamlama Eğlencelidir");
+    }
+}
